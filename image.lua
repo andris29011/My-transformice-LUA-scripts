d3a145655b7b7b59908c9b36c20945446e18cdf0
Hass <60549104+andris29011@users.noreply.github.com>
2021-01-22 13:45:09 +0100

    Create image.lua

---
+++ image.lua
@@ -0,0 +1,6 @@
+tfm.exec.newGame('@7825806')
+local a = tfm.exec.addImage(...)
+tfm.exec.removeImage(a)
+tfm.exec.addImage("17290c497e1.png", "%Villaaam#0000", -25, -25, nil)
+tfm.exec.addImage("1772553d614.png", "%Astagan#0000", -10, -25, nil)
+
